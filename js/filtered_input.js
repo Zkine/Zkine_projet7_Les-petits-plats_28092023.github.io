4077d61d7e09f2e97d0f33c4c82b1c68c4295914
Philippe JAYMES <106083377+Zkine@users.noreply.github.com>
2023-10-28 12:49:44 +0200

    Update filtered_input.js

--- js/filtered_input.js
+++ js/filtered_input.js
@@ -3,51 +3,31 @@ import { mediaIncrement } from "./index.js";
 //Fonction qui filtre les recettes en fonction du ou des caractères saisis par l'utilisateur puis
 // fait appel à la fonction mediaIncrement qui affichera les recettes recherchées
 
-// export const dataSearch = (result, data) => {
-//   let values = [];
-//   for (let repice = 0; repice < data.length; repice++) {
-//     if (
-//       result.includes(data[repice].name) ||
-//       result.includes(data[repice].description)
-//     ) {
-//       values.push(data[repice]);
-//     } else {
-//       for (
-//         let element = 0;
-//         element < data[repice].ingredients.length;
-//         element++
-//       ) {
-//         for (let n = 0; n < result.length; n++) {
-//           if (
-//             result[n] !== undefined &&
-//             result[n].toLowerCase() ===
-//               data[repice].ingredients[element].ingredient.toLowerCase()
-//           ) {
-//             values.push(data[repice]);
-//           }
-//         }
-//       }
-//     }
-//   }
-//   return mediaIncrement(values);
-// };
-
 export const dataSearch = (result, data) => {
-  const values = data
-    .map((recipe) => {
-      for (let val = 0; val < recipe.ingredients.length; val++) {
-        for (let el = 0; el < result.length; el++) {
+  let values = [];
+  for (let repice = 0; repice < data.length; repice++) {
+    if (
+      result.includes(data[repice].name) ||
+      result.includes(data[repice].description)
+    ) {
+      values.push(data[repice]);
+    } else {
+      for (
+        let element = 0;
+        element < data[repice].ingredients.length;
+        element++
+      ) {
+        for (let n = 0; n < result.length; n++) {
           if (
-            result.includes(recipe.name) ||
-            result.includes(recipe.description) ||
-            result[el].toLowerCase() ===
-              recipe.ingredients[val].ingredient.toLowerCase()
+            result[n] !== undefined &&
+            result[n].toLowerCase() ===
+              data[repice].ingredients[element].ingredient.toLowerCase()
           ) {
-            return recipe;
+            values.push(data[repice]);
           }
         }
       }
-    })
-    .filter((y) => y !== undefined);
+    }
+  }
   return mediaIncrement(values);
 };
